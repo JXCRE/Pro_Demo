c9bd54cd52508b4a0eacd3f25a1dc4df07584702
jtl <2541355327@qq.com>
2026-06-11 10:05:15 +0800

    fix:修改cmakelist文件、readme文件

---
+++ build/CMakeFiles/pro_demo.dir/src/main.c.obj.d
@@ -0,0 +1,15 @@
+CMakeFiles/pro_demo.dir/src/main.c.obj: D:\Desk\Template_demo\src\main.c \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ D:/C-TOOLS/Vscode/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ D:/Desk/Template_demo/include/main.h
--- readme.md
+++ readme.md
@@ -9,13 +9,29 @@
     C/C++
     CMake
     ...
-- Mingw下载 https://sourceforge.net/projects/mingw-w64/   
+- MinGW下载 https://sourceforge.net/projects/mingw-w64/   
     >1   
 - CMake下载 https://cmake.org/download/   
     >进入下载页面选择Binary distributions下的 *windows x64 installer* 或者 *windows x64 zip*   
     installer类似于安装包   
     zip需要配置系统环境变量(建议)   
+- 下载完成后，打开任意终端输入以下命令行，有版本输出，即环境搭建完成
+    ```shell
+    cmake --version
+    gcc --version
+    g++ --version
+    ``` 
+
 
 ## 如何进行编译调试   
 
+因为采用的是MinGW编译器，所以编译的时候要指定mingw的makefile生成器   
+
+```shell
+mkdir build
+cd build
+cmake -G "MinGW Makefiles" ..
+cd ..
+cmake --build build
+```
 
